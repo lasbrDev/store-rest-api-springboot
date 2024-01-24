9137d5e6f98530ec2229d8584135e11fcf3b965e
Luciano Alves <lasbr_alves@outlook.com>
2024-01-23 23:13:11 -0300

    Project started

---
+++ src/test/java/br/com/lasbr/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lasbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
